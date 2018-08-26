7791eaaae4d5feae212b3a6cdef6c8f52aaa3b74
Alex Harrison <alexjharrison@gmail.com>
2018-08-26 11:19:50 -0400

    changed callback to hernoku.us from localhost

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: `http://localhost:3000/auth/github/callback`
+  callbackURL: `http://hernoku.us/auth/github/callback`
 },
   function (accessToken, refreshToken, profile, done) {
     // User.findOrCreate({ githubId: profile.id }, function (err, user) {
